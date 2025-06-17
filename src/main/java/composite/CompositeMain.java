f3d49ac2d873147151a5e3bb46f7e4b6d820210c
REMR11 <ronaldmejia0211@gmail.com>
2025-06-16 19:29:51 -0600

    Revert "Feat: Modelos base para desing pattern Composite"
    This reverts commit 52586b572c3331089052506d087c72f353fb639a.

--- src/main/java/composite/CompositeMain.java
+++
@@ -1,7 +0,0 @@
-package composite;
-
-public class CompositeMain {
-    public static void main(String[] args) {
-
-    }
-}
--- src/main/java/composite/interfaces/UnidadPolicial.java
+++
@@ -1,5 +0,0 @@
-package composite.interfaces;
-
-public interface UnidadPolicial {
-    void mostrarDetalles();
-}
