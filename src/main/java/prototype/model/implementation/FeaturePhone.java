ba808f026668ef3dbb07284302768b8ad6753645
REMR11 <ronaldmejia0211@gmail.com>
2025-06-16 19:03:16 -0600

    Feat:Implementa interfaz telefono

--- src/main/java/prototype/model/implementation/FeaturePhone.java
+++ src/main/java/prototype/model/implementation/FeaturePhone.java
@@ -21,4 +21,37 @@ public class FeaturePhone implements Telefono {
             return null;
         }
     }
+
+    @Override
+    public String toString() {
+        return "FeaturePhone{" +
+                "marca='" + marca + '\'' +
+                ", modelo='" + modelo + '\'' +
+                ", capacidadBateria=" + capacidadBateria +
+                '}';
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getCapacidadBateria() {
+        return capacidadBateria;
+    }
+
+    public void setCapacidadBateria(int capacidadBateria) {
+        this.capacidadBateria = capacidadBateria;
+    }
 }
