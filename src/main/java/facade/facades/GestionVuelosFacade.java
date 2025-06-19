886f424293410d13789146921fba37ab6e6b9dac
REMR11 <ronaldmejia0211@gmail.com>
2025-06-18 18:26:27 -0600

    Feat: Creacion de fachada para consumo de servicios

--- src/main/java/facade/facades/GestionVuelosFacade.java
+++ src/main/java/facade/facades/GestionVuelosFacade.java
@@ -18,4 +18,18 @@ public class GestionVuelosFacade {
         this.sistemaPago = new  SistemaPago();
         this.sistemaReservas = new  SistemaReservas();
     }
+
+  public void reservarVuelos(String nombreCliente, String destino, String fecha, double pago){
+        if (sistemaDisponibilidadVuelos.verificarDisponibilidad(destino, fecha)){
+            sistemaReservas.crearReservas(nombreCliente, destino, fecha);
+            sistemaPago.procesoPago(nombreCliente, pago);
+            sistemaAsignacionAsiento.asignarAsiento(nombreCliente);
+
+            System.out.println("Reserva completada exitosamente para " + nombreCliente+ " para el dia "+ fecha);
+        }
+        else {
+            System.out.println("Lo siento, no tenemos asientos disponibles");
+        }
+  }
+
 }
